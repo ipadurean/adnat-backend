76473aba76abcfadba558547c9e852396176c873
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-11-10 09:05:38 -0600

    migrated and created first page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+    include ApplicationHelper
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:message] = @user.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+  
+  def edit
+    find_user
+  end
+
+  def update
+    find_user
+    if @user.update(user_params)
+      flash[:message] = "Your account has been successfully updated."
+      redirect_to @user
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :edit
+    end
+  end
+  
+  
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:organisation_id,:name, :password, :password_confirmation, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'users#new'
+  resources :shifts
+  resources :users
+  resources :organisations
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
